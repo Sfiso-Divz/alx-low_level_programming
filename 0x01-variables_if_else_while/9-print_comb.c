8b0065fff8da01ac94f17f788ee9bf5904dcfbd0
Sfiso-Divz <mfihlodlamini@gmail.com>
2023-02-17 13:23:36 -0800

    9-print_comb.c

---
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -0,0 +1,26 @@
+#include <stdlib.h>
+#include <time.h>
+#include <stdio.h>
+
+/**
+* main - Print all single digit numbers of base 10 starting from 0
+*Return: 0
+*/
+
+int main(void)
+{
+	int c = 0;
+
+	while (c < 10)
+	{
+		putchar(48 + c);
+		if (c != 9)
+		{
+			putchar(',')
+			putchar('');
+		}
+		c++;
+	}
+	putchar('\n');
+	return (0);
+}
