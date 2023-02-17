b610068cd0f0c64cdbe15229cbf355a744a204c7
Sfiso-Divz <mfihlodlamini@gmail.com>
2023-02-17 13:38:56 -0800

    100-print_comb3.c

---
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -0,0 +1,23 @@
+#include <stdlib.h>
+#include <time.h>
+#include <stdio.h>
+
+/**
+* main - Write a program that prints all possible different combinations of two digits.
+*Return: 0
+*/
+
+int main(void)
+{
+	int c;
+	int d = 0;
+
+	while (d < 10)
+	{
+		c = 0;
+
+		while (c < 10)
+		{
+			if (d != c && d < c)
+			{
+				putchar
