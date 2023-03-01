30d820ed11b1db712b31afdc5ac0d05db4735554
Sfiso-Divz <mfihlodlamini@gmail.com>
2023-03-01 11:22:49 -0800

    0-strcat.c

--- 0x06-pointers_arrays_strings/0-strcat.c
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -2,9 +2,9 @@
 
 /**
  * strcat- Concatenates the string pointed to by @src, including the terminat
- *	null byte, to the end of the string pointed to by @dest.
+ * null byte, to the end of the string pointed to by @dest.
  * @dest: A pointer to the string to be concatenated upon.
- * @src: The source string to be appended to @dest
+ * @src: The source string to be appended to @dest.
  * Return: A pointer to the destination string @dest.
  */
 
